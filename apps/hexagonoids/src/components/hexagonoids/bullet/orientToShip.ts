7473a7d216d3598863dc22b40c3b7ee639af135c
Grady Kuhnline <github@heygrady.net>
2023-07-28 10:41:21 -0700

    chore: move some commented code to the right spot

--- apps/hexagonoids/src/components/hexagonoids/bullet/orientToShip.ts
+++ apps/hexagonoids/src/components/hexagonoids/bullet/orientToShip.ts
@@ -7,35 +7,6 @@ import { accelerateHeadingSpeed } from '../ship/velocity'
 import { setLocation } from '../store/bullet/BulletSetters'
 import type { BulletStore } from '../store/bullet/BulletStore'
 
-// const getPlaneNormalFromSphere = (
-//   sphereCenter: Vector3,
-//   pointA: Vector3,
-//   pointB: Vector3
-// ): Vector3 => {
-//   const vectorA = pointA.subtract(sphereCenter)
-//   const vectorB = pointB.subtract(sphereCenter)
-//   const planeNormal = Vector3.Cross(vectorA, vectorB).normalize()
-
-//   return planeNormal
-// }
-
-// const pointAt = (pointA: Vector3, pointB: Vector3, t: number = 0) => {
-//   const dirA = pointA.normalize()
-//   const dirB = pointB.normalize()
-
-//   const greatCircleNormal = Vector3.Cross(dirA, dirB).normalize()
-//   const rotationA = Quaternion.FromLookDirectionLH(dirA, greatCircleNormal)
-//   const rotationB = Quaternion.FromLookDirectionLH(dirB, greatCircleNormal)
-
-//   const pathT = t
-//   const rotation = Quaternion.Slerp(rotationA, rotationB, pathT)
-//   const unitPathPoint = Vector3.Forward().rotateByQuaternionToRef(
-//     rotation,
-//     new Vector3()
-//   )
-//   return unitPathPoint.scaleInPlace(RADIUS)
-// }
-
 /**
  * Updates bullet state and bullet nodes.
  * @param $bullet
