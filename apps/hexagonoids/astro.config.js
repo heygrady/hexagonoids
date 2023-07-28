9023ae3269b1ed7b47c34d2cd09b13446812c2b7
Grady Kuhnline <github@heygrady.net>
2023-07-28 12:46:45 -0700

    fix: use static

--- apps/hexagonoids/astro.config.js
+++ apps/hexagonoids/astro.config.js
@@ -1,6 +1,6 @@
 import solid from '@astrojs/solid-js'
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/edge'
+import vercel from '@astrojs/vercel/static'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
