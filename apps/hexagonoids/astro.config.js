5d6b61a0672298b81c0868d5c862a706017c15f0
Grady Kuhnline <github@heygrady.net>
2023-07-28 12:41:06 -0700

    fix: configure astro vercel

--- apps/hexagonoids/astro.config.js
+++ apps/hexagonoids/astro.config.js
@@ -1,8 +1,12 @@
 import solid from '@astrojs/solid-js'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/edge'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solid(), tailwind()],
+  adapter: vercel({
+    analytics: true,
+  }),
 })
