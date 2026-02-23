dd196761b968138a7baa8c65aa376cfe883928a3
Grady Kuhnline <github@heygrady.net>
2026-02-22 23:27:33 -0800

    chore: add Biome linting and tsgo type-checking to monorepo toolchain
    - Add root biome.json with Biome formatter/linter config
    - Add lint-staged to husky pre-commit hook
    - Add check turbo pipeline task
    - Add .devlogs/ to .gitignore
    - Add check script to root package.json
    - Reformat commitlint.config.js and vscode launch.json with Biome
    - Update yarn.lock with new dependencies

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional', '@commitlint/config-workspace-scopes'],
+  extends: [
+    '@commitlint/config-conventional',
+    '@commitlint/config-workspace-scopes',
+  ],
 }
