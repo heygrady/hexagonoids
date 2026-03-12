5a11870bea05bb98e9df2871daabf604623c8a3d
Grady Kuhnline <github@heygrady.net>
2026-03-11 23:28:49 -0700

    refactor(tictactoe-demo): replace barrel re-export with explicit named exports
    - Replace `export * from './EvolutionManager.js'` with named exports for EvolutionManager
      and EvolutionManagerConfig
    - Add explicit named exports for algorithmRegistry (allActivations, getAlgorithmDefinition,
      AlgorithmDefinition, SupportedAlgorithm) and configDefaults (defaultEnvironmentConfig,
      defaultStrategyOptions, normalizationRanges)

--- packages/tictactoe-demo/src/index.ts
+++ packages/tictactoe-demo/src/index.ts
@@ -1,4 +1,18 @@
-export * from './EvolutionManager.js'
+export {
+  allActivations,
+  getAlgorithmDefinition,
+  type AlgorithmDefinition,
+  type SupportedAlgorithm,
+} from './algorithmRegistry.js'
+export {
+  defaultEnvironmentConfig,
+  defaultStrategyOptions,
+  normalizationRanges,
+} from './configDefaults.js'
+export {
+  EvolutionManager,
+  type EvolutionManagerConfig,
+} from './EvolutionManager.js'
 export * from './InteractiveGame.js'
 export * from './modulePathnames.js'
 export * from './types.js'
